9e1294ef32562f767192ee92bfb3093351aac3f9
RaleRaka <ratkorakarakic@gmail.com>
2022-02-05 00:41:51 +0100

    async

--- sketch-05.js
+++ sketch-05.js
@@ -5,6 +5,8 @@ const settings = {
 };
 
 
+let manager;
+
 let text = 'A';
 let fontSize = 1200;
 let fontFamily = 'serif';
@@ -44,4 +46,48 @@ const sketch = () => {
   };
 };
 
-canvasSketch(sketch, settings);
+
+
+const onKeyUp = (e) => {
+  text = e.key;
+  manager.render();
+};
+
+
+document.addEventListener('keyup', onKeyUp);
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+};
+
+start();
+
+
+
+
+// const url = 'https://picsum.photos/200';
+
+// const loadMeSomeImage = (url) => {
+//   return new Promise((resolve, reject) => {
+//     const img = new Image();
+//     img.onload = () => resolve(img);
+//     img.onerror = () => reject();
+//     img.src = url;
+//   });
+// };
+
+// const start = async () => {
+//   const img = await loadMeSomeImage(url)
+//   console.log('image width', img.width);
+//   console.log('this line');
+// };
+
+
+// const start = () => {
+//   loadMeSomeImage(url).then(img => {
+//     console.log('image width', img.width);
+//   });
+//   console.log('this line');
+// };
+
+// start();
